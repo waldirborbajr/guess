89cbad801af517d5c8d067f57d642350c877e5df
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2023-03-28 08:09:21 -0300

    chore(update): display message

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::{cmp::Ordering, io};
 use rand::Rng;
 
 fn main() {
-    let secgnm = rand::thread_rng().gen_range(1..11);
+    let secrete_number = rand::thread_rng().gen_range(1..11);
 
     println!("GMN - Guess My Number");
 
@@ -12,9 +12,7 @@ fn main() {
 
         let mut gmn = String::new();
 
-        io::stdin()
-            .read_line(&mut gmn)
-            .expect("Error reading number");
+        io::stdin().read_line(&mut gmn).expect( "Error reading file");
 
         // let gmn: u32 = gmn.trim().parse().expect("Type a number");
         let gmn: u32 = match gmn.trim().parse() {
@@ -24,7 +22,7 @@ fn main() {
 
         println!("You type: {}", gmn);
 
-        match gmn.cmp(&secgnm) {
+        match gmn.cmp(&secrete_number) {
             Ordering::Less => println!("Too low"),
             Ordering::Greater => println!("Too high"),
             Ordering::Equal => {
